91feb3097ebaed4deba77e57d3e343a725b5a65b
kaxie <kaxie@ucsd.edu>
2018-05-31 15:27:17 -0700

    加入了导航图，tabbar可行性低

--- pages/traffic/traffic.js
+++ pages/traffic/traffic.js
@@ -1,87 +1,65 @@
-// pages/result/retult.js
-
-var metrics;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    city:"",
-    touristSite:0,
-    relax:0,
-    saving:0,
-    time:0
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
     
-
-    this.setData({
-      city: options.metrics[0],
-      touristSite: options.metrics[1],
-      relax: options.metrics[2],
-      time: options.metrics[3],
-      saving: options.metrics[4], })
-      console.log(options.metrics);
-    metrics = {
-      city: this.data.city,
-      touristSite: this.data.touristSite,
-      relax: this.data.relax,
-      time: this.data.time
-    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    
   }
 })
