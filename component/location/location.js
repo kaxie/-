f03b66c01cc3741e9eec9f319909d83236167e80
kaxie <kaxie@ucsd.edu>
2018-05-31 20:12:02 -0700

    component updated

--- component/location/location.js
+++ component/location/location.js
@@ -1,3 +1,35 @@
 Component({
-  
+  properties:{
+    place:{
+      type:string,
+      value:""
+    },
+    inThePath:{
+      type: Boolean,
+      value: true
+    },
+    visitDuration:{
+      type: Intl,
+      value: 0
+    },
+    touristSite:{
+      type: Boolean,
+      value: true
+    }
+
+  },
+
+  data:{
+
+  },
+
+  attached: function(){
+
+  },
+  detached: function () {
+
+  },
+  methods:{
+    
+  }
 })
